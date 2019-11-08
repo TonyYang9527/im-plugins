a12ec35e9b159396d3439449ef5b6e6655297e41
tony.yang <tony.yang@bwoil.com>
2019-11-08 17:35:04 +0800

    add livechat componet

--- src/livechat/LiveChat/index.js
+++ src/livechat/LiveChat/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { observer } from "mobx-react";
-import { toggleChat, addUserMessage } from '@actions';
 import WidgetLayout from './layout';
 
 class LiveChat extends Component {
